3f1b9cababadfd4b6a006b49e16c6d90268261fe
Valetina-2608 <valusha2608@gmail.com>
2022-12-30 18:21:12 +0200

    Add tours.js

---
+++ tours.js
@@ -0,0 +1,44 @@
+tours = [
+    {
+        "id":1,
+        "image":"img/",
+        "caption": "Kracow Landmarks",
+        "duration":"1 day",
+        "information": "Kracow the ancient seat of Polish kings and intelligentsia, a city of legend and myth that evokes the most fanciful of images, from dragons under the catacombs of Wawel, to Tartar hordes repelled at the gates, to pigeon-knights waiting for their king to return. Kraków’s postcard-perfect Old Town comes complete with Europe’s largest medieval market square and even fairy-tale Wawel Castle overlooking the river. Discover its main landmarks during one day full of beauty."
+    },
+    {
+        "id":2,
+        "image":"img/",
+        "caption": "Colosseum Tour",
+        "duration":"1 day",
+        "information": "What sums up Rome more than an insider's look into its most famous monument? The Colosseum conjures excitement in all who set eyes on it. From its massive size to purpose, it is both harrowing and graceful to behol"
+    },
+    {
+        "id":3,
+        "image":"img/",
+        "caption": "Venice Tour",
+        "duration":"1 day",
+        "information": "rom the iconic Doge's Palace to the stunning St. Mark Basilica our expert guides, we will make your vacation in Venice one to remember."
+    },
+    {
+        "id":4,
+        "image":"img/",
+        "caption": "Versailles Day Trip",
+        "duration":"1 day",
+        "information": "Palace of Versailles, former French royal residence and centre of government, now a national landmark. It is located in the city of Versailles, Yvelines département, Île-de-France région, northern France, 10 miles (16 km) west-southwest of Paris. "
+    },
+    {
+        "id":5,
+        "image":"img/",
+        "caption": "Louvre Tour",
+        "duration":"1 day",
+        "information": "Discover the world's most visited art museum and see why this vast collection spanning the Ancient Egyptian to the Neoclassical attracts so many visitors each year. No other museum sums up humankind's artistic endeavor more, but the Mona Lisa and Venus de Milo are only the beginning of what you'll see on our Louvre tours."
+    },
+    {
+        "id":6,
+        "image":"img/",
+        "caption": "Tower of London Tour",
+        "duration":"1 day",
+        "information": "Tower of London, byname the Tower, royal fortress and London landmark. Its buildings and grounds served historically as a royal palace, a political prison, an arsenal and a public records office. It is located on the north bank of the River Thames."
+    }
+]
